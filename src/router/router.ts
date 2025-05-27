16588637fd8356dab1e8b293ec8dde212ffe6435
lenny <lennyrappro@gmail.com>
2025-05-27 17:53:11 +0200

    ✨Update Router

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,36 +1,42 @@
-import {createRouter, createWebHistory} from "vue-router"
-import type { RouteRecordRaw } from "vue-router"
-import HomePage from "../views/HomePage.vue"
-import Album from "@/views/Album.vue"
-import History from "@/views/History.vue"
-import Style from "@/views/Style.vue"
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+import HomePage from '../views/HomePage.vue'
+import Album from '@/views/Album.vue'
+import AlbumDetail from '@/views/AlbumDetail.vue'
+import History from '@/views/History.vue'
+import Style from '@/views/Style.vue'
 
 const routes: RouteRecordRaw[] = [
-    {
-        path: '/',
-        name: "Home",
-        component: HomePage
-    },
-    {
-        path: '/album',
-        name: "Album",
-        component: Album
-    },
-    {
-        path: '/history',
-        name: "History",
-        component: History
-    },
-    {
-        path: '/style',
-        name: "Style",
-        component: Style
-    },
+  {
+    path: '/',
+    name: 'Home',
+    component: HomePage
+  },
+  {
+    path: '/album',
+    name: 'Album',
+    component: Album
+  },
+  {
+    path: '/album/:id',
+    name: 'AlbumDetail',
+    component: AlbumDetail
+  },
+  {
+    path: '/history',
+    name: 'History',
+    component: History
+  },
+  {
+    path: '/style',
+    name: 'Style',
+    component: Style
+  }
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes
 })
 
 export default router
